deaea49aa7bfca125173bf9ff49b4d46412e1476
Quentin Crida <qcrida@gmail.com>
2020-01-27 18:07:02 +0000

    printer start

---
+++ src/main/java/Printer.java
@@ -0,0 +1,15 @@
+public class Printer {
+
+    private int sheetsLeft;
+
+    public Printer(int sheetsLeft) {
+        this.sheetsLeft = sheetsLeft;
+    }
+}
+
+    public int pageCount(int pages, int copies) {
+            return (pages, copies);
+}
+}
+
+
---
+++ src/test/java/PrinterTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class PrinterTest {
+    private Printer printer;
+
+    @Before
+    public void before() {
+        printer = new Printer();
+    }
+    @Test
+    public void pageCount(int pages, int copies)
+}
+
+
