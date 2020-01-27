32a02f03474a5ad68d86f8d2383e3fa400636e9f
Quentin Crida <qcrida@gmail.com>
2020-01-27 21:23:51 +0000

    printer sheetsLeft test

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,15 +1,25 @@
 public class Printer {
 
     private int sheetsLeft;
+//    private int pages;
+//    private int copies;
+
 
     public Printer(int sheetsLeft) {
         this.sheetsLeft = sheetsLeft;
     }
-}
+    public int getsheetsLeft() {
+         return sheetsLeft;
+    }
 
-    public int pageCount(int pages, int copies) {
-            return (pages, copies);
-}
-}
+    }
+
+//    public int pageCount(int pages, int copies) {
+//            return pages;
+
+
+
+
+
 
 
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,15 +1,27 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
     private Printer printer;
 
     @Before
     public void before() {
-        printer = new Printer();
+        printer = new Printer(100);
     }
     @Test
-    public void pageCount(int pages, int copies)
+    public void sheetsLeft(){
+        assertEquals(100, printer.getsheetsLeft());
+    }
 }
+//
+//    @Test
+//    public void canAdd() {
+//        assertEquals(9, calculator.add(5, 4));
+//    }
 
-
+//    @Test
+//    public void pageCount()
+//    assertEquals((2,2), printer.pageCount(2,2))
+//}
